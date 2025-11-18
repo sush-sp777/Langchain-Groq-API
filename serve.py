325d8fdf637524fc559cfc8cd649a87d2b5cd490
sush-sp777 <sushantsp433@gmail.com>
2025-11-18 13:58:28 +0530

    initial commit

---
+++ serve.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+
+model=ChatGroq(model='llama-3.1-8b-instant',groq_api_key=groq_api_key)
+
+#Create prompt template
+system_template="Translate the following into {language}:"
+prompt_template=ChatPromptTemplate.from_messages([
+    ('system',system_template),
+    ('user','{text}')
+])
+
+parser=StrOutputParser()
+
+#create chain
+chain=prompt_template|model|parser
+
+#App defination
+app=FastAPI(title="Langchain Server",
+            version="1.0",
+            description="A simple API server using langchain runnable interfaces")
+
+#adding chain routes
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
+
